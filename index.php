eeb4860bf0feaf35db1dda31c02076ed83ffcca3
Amir Hamzeh <123162678+AmirHamzeSh@users.noreply.github.com>
2023-02-20 15:26:59 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>AmirHamzeSh</title>
+    <meta charset="UTF-8">
+ </head>
+  
+  <body>
+    <h3>Hello to My GitHub Page</h3>
+  </body>
+  
+  </html>
